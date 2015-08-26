f3fc3b0bc2910719aa7078f259999d3a3d3413d6
周亮 <zhouliang@zhouliangdeMacBook-Pro.local>
2015-08-26 17:31:38 +0800

    Initial Commit

---
+++ Weixin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weixin
+//
+//  Created by 周亮 on 15/8/26.
+//  Copyright (c) 2015年 周亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Weixin/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Weixin
+//
+//  Created by 周亮 on 15/8/26.
+//  Copyright (c) 2015年 周亮. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
